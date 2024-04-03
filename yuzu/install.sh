060018dcc23323ddf7487e5074b52fa53a5459f0
Csaba Gaspar <cgaspar@accretivetg.com>
2024-04-03 07:45:49 +0200

    update

--- yuzu/install.sh
+++ yuzu/install.sh
@@ -8,19 +8,21 @@ source $PARENT_PATH/yuzu.env
 
 #Copy image
 mkdir -p $APPS_DIR
-cp $YUZU_DIR/$APP_NAME $APPS_DIR
+cp $INSTALLS_DIR/yuzu/$APP_NAME $APPS_DIR
 chmod a+x $APPS_DIR/$APP_NAME
 
 cp $YUZU_DIR/yuzu.sh $APPS_DIR
 
 #~/.local/share/yuzu
 mkdir -p $HOME/.local/share/yuzu/keys
-cp $YUZU_DIR/configs/keys/*.keys $HOME/.local/share/yuzu/keys
+cp $INSTALLS_DIR/yuzu/keys/*.keys $HOME/.local/share/yuzu/keys
 
 #~/.local/share/yuzu/nand/system/Contents/registered
-#mkdir -p $HOME/.local/share/yuzu/nand/system/Contents/registered
-#TODO unzip and copy
+mkdir -p $HOME/.local/share/yuzu/nand/system/Contents/registered
+unzip $INSTALLS_DIR/yuzu/firmware17_0_1.zip -d $HOME/.local/share/yuzu/nand/system/Contents/registered
 
 #~/.config/yuzu
 mkdir -p $HOME/.config/yuzu
+mkdir -p $HOME/.config/yuzu/input
 cp $YUZU_DIR/configs/qt-config.ini $HOME/.config/yuzu
+cp $YUZU_DIR/configs/input/steamdeck.ini $HOME/.config/yuzu/input
